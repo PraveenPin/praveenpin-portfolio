93dc82767db6e8386dba9f9b79fe8ea4f338f229
Praveen Pinjala <mrpin@nbp-155-117.nbp.ruw.rutgers.edu>
2021-09-16 12:52:51 -0400

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Footer = (props) => {
+  const bgStyle = { backgroundColor: "#f5f5f5" };
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+      <Container style={{ color: '#848c92'}}>
+        {props.children}
+        <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
+        <a
+          className="badge badge-dark"
+          rel="noopener"
+          href="https://github.com/PraveenPin"
+          aria-label="My GitHub"
+        >
+          Praveen Pinjala
+        </a>{" "}
+        using <i className="fab fa-react" />
+        <p>
+          <small className="text-muted">
+            Project code is open source. Feel free to fork and make your own
+            version.
+          </small>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
